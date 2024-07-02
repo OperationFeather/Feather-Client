6254f2f3a9b72d7085534bf60e100c6335fb5075
Panda <126839661+PandaHubRoblox@users.noreply.github.com>
2024-07-02 09:26:58 -0400

    Update ESPLibrary.lua

--- Games/Utils/ESPLibrary.lua
+++ Games/Utils/ESPLibrary.lua
@@ -7,20 +7,11 @@ local camera = workspace.CurrentCamera
 local cache = {}
 
 local bones = {
-    {"Head", "UpperTorso"},
-    {"UpperTorso", "RightUpperArm"},
-    {"RightUpperArm", "RightLowerArm"},
-    {"RightLowerArm", "RightHand"},
-    {"UpperTorso", "LeftUpperArm"},
-    {"LeftUpperArm", "LeftLowerArm"},
-    {"LeftLowerArm", "LeftHand"},
-    {"UpperTorso", "LowerTorso"},
-    {"LowerTorso", "LeftUpperLeg"},
-    {"LeftUpperLeg", "LeftLowerLeg"},
-    {"LeftLowerLeg", "LeftFoot"},
-    {"LowerTorso", "RightUpperLeg"},
-    {"RightUpperLeg", "RightLowerLeg"},
-    {"RightLowerLeg", "RightFoot"}
+      {"Head", "Torso"},
+    {"Torso", "Left Arm"},
+    {"Torso", "Right Arm"},
+    {"Torso", "Left Leg"},
+    {"Torso", "Right Leg"}
 }
 
 --// Settings
