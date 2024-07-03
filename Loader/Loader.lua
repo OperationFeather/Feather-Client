df51cab22f2155fc93b150fa03921534435083b1
Panda <126839661+PandaHubRoblox@users.noreply.github.com>
2024-07-03 09:09:08 -0400

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,4 +1,4 @@
-local Link = '%s.lua'
+local Link = 'https://raw.githubusercontent.com/OperationFeather/Feather-Client/main/Games/%s.lua'
 local True,False = pcall(function()
     loadstring(game:HttpGet(Link:format(tostring(game.PlaceId)), true))
 end) 
